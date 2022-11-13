520782a3683b36c190b2ea9f72d1149697c2cc17
rwchua <rwchua@umich.edu>
2022-11-12 21:43:30 -0500

    Completed alumnize draft

--- commands/alumnize.js
+++ commands/alumnize.js
@@ -16,29 +16,66 @@ module.exports = {
 	maxArgs: 2,
 	options: [
 		{
-		  name: 'start_date',
-		  description: 'Start date of Student Alumni',
-		  required: true,
-		  type: ApplicationCommandOptionType.String,
+			name: 'start_date',
+			description: 'Provide a date (YYYY-MM-DD)',
+			required: true,
+			type: ApplicationCommandOptionType.String,
 		},
 		{
 			name: 'end_date',
-			description: 'End date of Student Alumni',
+			description: 'Provide a date (YYYY-MM-DD)',
 			required: true,
 			type: ApplicationCommandOptionType.String,
-		  },
-	  ],
+		},
+	],
 	expectedArgs: "<[YYYY-MM-DD] [YYY-MM-DD]>",
 	description: 'Assigns students from start date to end date with the Student Alumni Role',
 	testOnly: true, //so the slash command updates instantly
+	// init: (client) => { //For automagic detections
+	// 	setTimeout(() => {
+	// 		const now = new Date;
+	// 		if (now.getDate() == 1 && (now.getMonth() == 0 || now.getMonth() == 4 || now.getMonth() == 6)) {
+	// 			//getDate() is 1-indexed but getMonth() is 0-indexed
+	// 			now.setDate(2); //So we get on or before today!
+	// 			client.guilds.fetch('734492640216744017').then(g => {
+	// 				g.members.fetch().then(u => { //Cache all members
+	// 					g.roles.fetch(studentRole).then(r => { //Extract members with the Student role
+	// 						r.members.each(user => { //r.members is a Collection<userID, GuildMember>
+	// 							if (user.joinedAt < now) {
+	// 								user.roles.add('748920659626950737'); //Targets bots for safety
+	// 								++count;
+	// 							}
+	// 						});
+	// 						g.channels.fetch('749407788156846162').then(c => { //#bot-commands
+	// 							c.send(`Students automatically alumnized: ${count}.`);
+	// 						})
+	// 					});
+	// 				})
+	// 				.catch(console.error);
+	// 			})
+	// 		}
+	// 	}, 1000 * 60 * 60 * 24);
+	// },
 	callback: async ({ guild, args, interaction: msgInt }) => {
-		const start = new Date(args[0] += "T12:00");
-		const end = new Date(args[1] += "T12:00");
-		guild.members.fetch().then(u => { //Cache all members
-			guild.roles.fetch(studentRole).then(r => { //Extract members with the Student role
-				console.log(r.members.size); //r.members is a Collection<userID, GuildMember>
-			});
-		})
-		.catch(console.error);
+		if (args[0].length != 10 || args[1].length != 10) { //Basic error checking. Not perfect.
+			msgInt.reply(`Please make sure dates are of the form YYYY-MM-DD`);
+			return;
+		} else {
+			const start = new Date(args[0] += "T12:00");
+			const end = new Date(args[1] += "T12:00");
+			let count = 0;
+			guild.members.fetch().then(u => { //Cache all members
+				guild.roles.fetch('749343797623783575').then(r => { //Extract members with the Student role
+					r.members.each(user => { //r.members is a Collection<userID, GuildMember>
+						if (user.joinedAt > start && user.joinedAt < end) {
+							user.roles.add('748920659626950737'); //Targets bots for safety
+							++count;
+						}
+					});
+					msgInt.reply(`Students Alumnized: ${count}.`);
+				});
+			})
+			.catch(console.error);
+		}
 	}
 };
