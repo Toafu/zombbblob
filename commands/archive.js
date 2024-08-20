285c5aaa390371c0031a983247742f6442aa8382
Ethan Hardy <102838865+978-9-17-637879-3@users.noreply.github.com>
2024-08-20 19:37:21 -0400

    improve archive
    condense multi-line variable assignments
    remove set position workaround
    defer reply (due to potential ratelimiting)

--- commands/archive.js
+++ commands/archive.js
@@ -19,31 +19,35 @@ module.exports = {
 	description: 'archives the requested channel category',
 	testOnly: true, //so the slash command updates instantly
 	callback: async ({ guild, text, interaction: msgInt }) => {
+		const deferredReply = await msgInt.deferReply();
+
 		const semester = text.toLowerCase();
 		if (semester.length === 3 && (semester.startsWith('f') || semester.startsWith('s') || semester.startsWith('w'))) {
-			let categoryName = Semesters[semester.at(0)];
-			let year = semester.slice(1); //Remove first character
-			year = year.padStart(4, '20'); //Pads up to 4 character string
-			categoryName = categoryName + ` ${year}`;
+			const year = semester.slice(1).padStart(4, '20'); //Remove first character, pad YY into YYYY
+			const categoryName = `${Semesters[semester.at(0)]} ${year}`;
+
 			const channels = await guild.channels.fetch();
 			const categoryToArchive = channels.find(cat => cat.type === ChannelType.GuildCategory && cat.name === categoryName);
 			if (!categoryToArchive) {
-				msgInt.reply(`Unable to find category called \`${categoryName}\``);
+				await deferredReply.edit(`Unable to find category called \`${categoryName}\``);
 				return;
 			}
+
 			await categoryToArchive.setName(`${categoryName} (Archived)`);
+
 			await categoryToArchive.permissionOverwrites.edit(Student, { SendMessages: false });
 			await categoryToArchive.permissionOverwrites.edit(StudentAlumni, { SendMessages: false });
-			await categoryToArchive.children.cache.each(c => {
-				c.lockPermissions();
-			});
-			const t = await guild.channels.fetch(Channels.smallstudyrooms);
-			// Attempting to put a "higher" category below another actually puts it two below the target
-			await categoryToArchive.edit({position: t.rawPosition + 1});
-			await categoryToArchive.edit({position: categoryToArchive.position - 1});
-			msgInt.reply(`Successfully archived ${categoryName}`);
+
+			for (const [_, childChannel] of categoryToArchive.children.cache) {
+				await childChannel.lockPermissions();
+			}
+
+			const highestNonArchiveChannel = await guild.channels.fetch(Channels.smallstudyrooms);
+			await categoryToArchive.setPosition(highestNonArchiveChannel.position + 1, { reason: "/archive" });
+
+			deferredReply.edit(`Successfully archived ${categoryName}`);
 		} else {
-			msgInt.reply('Invalid semester format. Must follow `[F/S/W][Last two digits of year]`');
+			deferredReply.edit('Invalid semester format. Must follow `[F/S/W][Last two digits of year]`');
 		}
 	}
 };
