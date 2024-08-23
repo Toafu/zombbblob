b0a09998a1cd96ec1b8d3562b950637295134371
Ethan Hardy <102838865+978-9-17-637879-3@users.noreply.github.com>
2024-08-23 16:05:25 -0400

    guard clause

--- src/commands/assign.ts
+++ src/commands/assign.ts
@@ -28,13 +28,15 @@ export const command: Command = {
 			await interaction.reply("Failed to get target from cache");
 			return;
 		}
-		const interactionMember = await interaction.guild.members.fetch(interaction.user.id);
 
+		const interactionMember = await interaction.guild.members.fetch(interaction.user.id);
+		
 		if (targetRole.comparePositionTo(interactionMember.roles.highest) > 0 && !interactionMember.permissions.has(PermissionsBitField.Flags.Administrator)) {
 			await interaction.reply(`<:blobdisapproval:1039016273343951009> You cannot assign a role that is higher than your highest role (Administrators can bypass).`);
-		} else {
-			await targetMember.roles.add(targetRole);
-			await interaction.reply(`Successfully assigned the ${targetRole.name} role to ${targetMember.user.username}`);
+			return;
 		}
+
+		await targetMember.roles.add(targetRole);
+		await interaction.reply(`Successfully assigned the ${targetRole.name} role to ${targetMember.user.username}`);
 	},
 };
