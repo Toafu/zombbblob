7eb8751a1719d2a7ea8d57977bea7f6e5cd9534a
Ethan Hardy <102838865+978-9-17-637879-3@users.noreply.github.com>
2024-08-24 15:55:17 -0400

    format 1

--- src/commands/lock.ts
+++ src/commands/lock.ts
@@ -3,25 +3,26 @@ import { Roles, communicationsPermissions } from '../utils';
 import { Command } from '../command';
 
 export const command: Command = {
-	data: new SlashCommandBuilder()
-		.setName('lock')
-		.setDescription('locks the server to Students (unable to communicate)'),
-	init: () => {},
-	execute: async (interaction: ChatInputCommandInteraction) => {
-		if (interaction.guild === null) {
-			return;
-		}
+    data: new SlashCommandBuilder()
+        .setName('lock')
+        .setDescription('locks the server to Students (unable to communicate)'),
+    init: () => {},
+    execute: async (interaction: ChatInputCommandInteraction) => {
+        if (interaction.guild === null) {
+            return;
+        }
 
-		const studentRole = await interaction.guild.roles.fetch(Roles.Student);
-		if (studentRole === null) {
-			await interaction.reply("Could not fetch Student role");
-			return;
-		}
+        const studentRole = await interaction.guild.roles.fetch(Roles.Student);
+        if (studentRole === null) {
+            await interaction.reply('Could not fetch Student role');
+            return;
+        }
 
-		const resultMessage = await studentRole.setPermissions(studentRole.permissions.remove(communicationsPermissions))
-			.then(() => "Server locked to the Student role")
-			.catch(() => "Unable to remove permissions from Student");
+        const resultMessage = await studentRole
+            .setPermissions(studentRole.permissions.remove(communicationsPermissions))
+            .then(() => 'Server locked to the Student role')
+            .catch(() => 'Unable to remove permissions from Student');
 
-		await interaction.reply(resultMessage);
-	}
+        await interaction.reply(resultMessage);
+    },
 };
--- src/commands/unlock.ts
+++ src/commands/unlock.ts
@@ -3,25 +3,26 @@ import { communicationsPermissions, Roles } from '../utils';
 import { Command } from '../command';
 
 export const command: Command = {
-	data: new SlashCommandBuilder()
-		.setName('unlock')
-		.setDescription('unlocks the server to Students (able to communicate)'),
-	init: () => {},
-	execute: async (interaction: ChatInputCommandInteraction) => {
-		if (interaction.guild === null) {
-			return;
-		}
+    data: new SlashCommandBuilder()
+        .setName('unlock')
+        .setDescription('unlocks the server to Students (able to communicate)'),
+    init: () => {},
+    execute: async (interaction: ChatInputCommandInteraction) => {
+        if (interaction.guild === null) {
+            return;
+        }
 
-		const studentRole = await interaction.guild.roles.fetch(Roles.Student);
-		if (studentRole === null) {
-			await interaction.reply("Could not fetch Student role");
-			return;
-		}
+        const studentRole = await interaction.guild.roles.fetch(Roles.Student);
+        if (studentRole === null) {
+            await interaction.reply('Could not fetch Student role');
+            return;
+        }
 
-		const resultMessage = await studentRole.setPermissions(studentRole.permissions.add(communicationsPermissions))
-			.then(() => "Server unlocked to the Student role")
-			.catch(() => "Unable to add permissions to Student");
+        const resultMessage = await studentRole
+            .setPermissions(studentRole.permissions.add(communicationsPermissions))
+            .then(() => 'Server unlocked to the Student role')
+            .catch(() => 'Unable to add permissions to Student');
 
-		await interaction.reply(resultMessage);
-	}
+        await interaction.reply(resultMessage);
+    },
 };
