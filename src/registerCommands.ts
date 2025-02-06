36f55de778fffb074f2bd0e34ee0490a36c55199
Ethan Hardy <102838865+978-9-17-637879-3@users.noreply.github.com>
2025-02-05 22:51:09 -0500

    Revert "temp debug"
    This reverts commit fb104f1549f0d8255ac39b2c4df2fa3bdb8a6e68.

--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -9,6 +9,7 @@ const { CLIENT_ID, SERVER_ID } = ConfigHandler.getInstance().getConfig();
 
 export const registerCommands = async function() {
     console.log("Registering commands...");
+
     if (!process.env.TOKEN) {
         console.error("No TOKEN in ENV!");
         process.exit(1);
@@ -24,8 +25,6 @@ export const registerCommands = async function() {
         return command.data.toJSON();
     }));
 
-    restClient.on("rateLimited", console.log)
-
     console.log("Clearing old commands...");
     await restClient.put(
         Routes.applicationGuildCommands(CLIENT_ID, SERVER_ID), 
@@ -33,10 +32,6 @@ export const registerCommands = async function() {
     );
     
     console.log("Inserting new commands...");
-    console.log(JSON.stringify(commandDatums))
-    console.log(CLIENT_ID) 
-    console.log(SERVER_ID)
-    console.log(process.env.TOKEN);
     await restClient.put(
         Routes.applicationGuildCommands(CLIENT_ID, SERVER_ID),
         { body: commandDatums }
