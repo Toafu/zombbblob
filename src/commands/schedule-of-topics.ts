fa6fcc3016c06bbfa1b033cb87569c32acfbcd54
Ryan Chua <rwchua@umich.edu>
2025-02-06 23:44:06 -0500

    Add sot command

---
+++ src/commands/schedule-of-topics.ts
@@ -0,0 +1,16 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { Command } from '../command';
+
+export const command: Command = {
+	data: new SlashCommandBuilder()
+		.setName('schedule-of-topics')
+		.setDescription('sends the schedule of topics'),
+	init: () => {},
+	execute: async (interaction: ChatInputCommandInteraction) => {
+		if (interaction.guild === null) {
+			return;
+		}
+		const deferredReply = await interaction.deferReply();
+		deferredReply.edit("[Here's the schedule of topics!](https://docs.google.com/spreadsheets/d/1WB25XRF0XTL5HOgDNSinp4cHZN_pxxpetD_qj-PboqQ)");
+	}
+};
