71fd2b3d21bde07a5afed1228f0d6eed97f98d59
Julianne Park <yeonjay@umich.edu>
2025-02-06 22:45:43 -0500

    New command to fetch infected word implemented.

---
+++ src/commands/infected-word.ts
@@ -0,0 +1,27 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Command } from "../command";
+
+import { WordsDatabase } from "../db";
+
+import { ConfigHandler } from "../config";
+const { Channels, Roles } = ConfigHandler.getInstance().getConfig();
+
+export const command: Command = {
+	data: new SlashCommandBuilder()
+		.setName('infected-word')
+		.setDescription('sends the current infected word'),
+	init: () => {},
+	authorizedRoleIDs: [Roles.InfectedZombbblob],
+	permittedChannelIDs: [Channels.zombbblob],
+	execute: async (interaction: ChatInputCommandInteraction) => {
+		if (interaction.guild === null) {
+			return;
+		}
+
+		const deferredReply = await interaction.deferReply();
+
+		const db = WordsDatabase.getInstance();
+		const infectedWord = db.getInfectedWord();
+		await deferredReply.edit(`The infected word is \`${infectedWord}\``);
+	}
+};
