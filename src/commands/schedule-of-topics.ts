a559728c9267ef1b975379d2aa8e81da4bc4ef27
Ethan Hardy <102838865+hardy-ethan@users.noreply.github.com>
2025-02-10 16:23:32 -0500

    feat: allow students to use schedule of topics command

--- src/commands/schedule-of-topics.ts
+++ src/commands/schedule-of-topics.ts
@@ -1,11 +1,15 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { Command } from '../command';
 
+import { ConfigHandler } from '../config';
+const { Roles } = ConfigHandler.getInstance().getConfig();
+
 export const command: Command = {
 	data: new SlashCommandBuilder()
 		.setName('schedule-of-topics')
 		.setDescription('sends the schedule of topics'),
 	init: () => {},
+	authorizedRoleIDs: [Roles.Student],
 	execute: async (interaction: ChatInputCommandInteraction) => {
 		if (interaction.guild === null) {
 			return;
