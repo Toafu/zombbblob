8d7d7910328df5169b3a75b34d5304b205509d05
Ryan Chua <71936834+Toafu@users.noreply.github.com>
2025-02-12 20:10:07 -0500

    Add get-all-words command (#96)

---
+++ src/commands/get-all-words.ts
@@ -0,0 +1,32 @@
+import { AttachmentBuilder, ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Command } from "../command";
+import { WordsDatabase } from "../db";
+
+import { ConfigHandler } from "../config";
+const { Channels } = ConfigHandler.getInstance().getConfig();
+
+export const command: Command = {
+	data: new SlashCommandBuilder()
+		.setName("get-all-words")
+		.setDescription("extract word list into a text file"),
+	permittedChannelIDs: [Channels.staff_bot_commands, Channels.zombbblob_trolling],
+	init: () => { },
+	execute: async (interaction: ChatInputCommandInteraction) => {
+		if (!interaction.guild) {
+			return;
+		}
+
+		const words = WordsDatabase.getInstance().getAllWords().map(w => w.word).join('\n');
+
+		if (words.length === 0) {
+			await interaction.reply("There are no infected words!");
+			return;
+		}
+
+		const file = new AttachmentBuilder(Buffer.from(words), {
+			name: "words.txt", description: "current infected word list"
+		});
+
+		await interaction.reply({content: 'Here is the current word list!', files: [file]});
+	},
+};
