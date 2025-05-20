b815b2fce22d11d5e1ad11e6f72a741929c43942
Ethan Hardy <102838865+hardy-ethan@users.noreply.github.com>
2025-05-20 18:05:33 -0400

    docs: explain Zip regex (#142)

--- src/games/zipgame.ts
+++ src/games/zipgame.ts
@@ -18,6 +18,22 @@ interface ZipParseOptions {
 
 const zipParseOptionsArr: ZipParseOptions[] = [
     {   // Generic (logged-out and most languages)
+        /*
+
+        * Zip\D*?(\d+) - matches the Zip number section
+            * Zip - matches name
+            * \D*? - matches text between name and number
+            * (\d+) - captures actual number
+        * [\S\s]*? - matches any text between previous and the time
+        * (\d+):(\d+) - captures the minutes and seconds
+        * \D*? - matches all text until the backtracks
+        * (?:\D*?(\d*)\D*?(🛑|🟢))? - optional non-capturing group for backtracks
+            * \D*? - matches text before backtracks
+            * (\d*) - captures the backtrack count (or empty if none)
+            * \D*? - matches text after backtrack count (if any)
+            * (🛑|🟢) - captures colored emoji (🛑 iff backtracks)
+        
+        */
         regex: /^Zip\D*?(\d+)[\S\s]*?(\d+):(\d+)\D*?(?:\D*?(\d*)\D*?(🛑|🟢))?/mu,
         groups: {
             zipNumber: 1,
