ca013c0b4554caecc1313016a7989d267d3e6792
Ethan Hardy <102838865+hardy-ethan@users.noreply.github.com>
2025-06-05 10:45:44 +0000

    Revert "refactor: organize (#162)"
    This reverts commit 7ecf754a3bbe814f1fbbfda1ab20c3850b4a86dd.

---
+++ src/commands/games/zombbblob/infected-word.ts
@@ -0,0 +1,38 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Command } from "../../../command";
+
+import { WordsDatabase } from "../../../games/zombbblobdb";
+
+import { ConfigHandler } from "../../../config";
+import { maintainersPingString } from "../../../utils";
+const { Channels, Roles } = ConfigHandler.getInstance().getConfig();
+
+export const command: Command = {
+	data: new SlashCommandBuilder()
+		.setName('infected-word')
+		.setDescription('sends the current infected word'),
+	init: () => {},
+	authorizedRoleIDs: [Roles.InfectedZombbblob],
+	permittedChannelIDs: [Channels.zombbblob],
+	execute: async (interaction: ChatInputCommandInteraction) => {
+		if (interaction.guild === null) {
+			return;
+		}
+
+		const db = WordsDatabase.getInstance();
+
+		if (!db.isGameRunning()) {
+			await interaction.reply({content: `The zombbblob minigame isn't currently running!`, ephemeral: true});
+			return;
+		}
+
+		const infectedWord = db.getInfectedWord();
+		
+		if (infectedWord === null) {
+			await interaction.reply(`I was unable to get the infected word! ${maintainersPingString} pls fix`);
+			return;
+		}
+
+		await interaction.reply(`The infected word is \`${infectedWord}\``);
+	}
+};
---
+++ src/commands/games/zombbblob/remove-word.ts
@@ -0,0 +1,28 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Command } from "../../../command";
+
+import { WordsDatabase } from "../../../games/zombbblobdb";
+
+export const command: Command = {
+	data: new SlashCommandBuilder()
+		.setName('remove-word')
+		.addStringOption(option => option
+			.setName('word')
+			.setDescription('A word to no longer be eligible for infection')
+			.setRequired(true))
+		.setDescription('removes a word from the zombbblob minigame'),
+	init: () => {},
+	execute: async (interaction: ChatInputCommandInteraction) => {
+		if (!interaction.guild) {
+			return;
+		}
+
+		const word = interaction.options.getString('word', true);
+		const success = WordsDatabase.getInstance().removeWord(word);
+		if (success) {
+			await interaction.reply(`Successfully deleted \`${word}\` from the zombbblob minigame`);
+		} else {
+			await interaction.reply(`Unable to delete \`${word}\` from the zombbblob minigame (the word doesn't exist or is currently infected)`);
+		}
+	}
+};
